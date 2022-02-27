d08d37117aef9b27baa824dd1de03e46a31c13be
mpsloan <97258032+mpsloan@users.noreply.github.com>
2022-02-27 17:38:34 -0500

    Update GeneticAlgorithm.java

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -1,15 +1,3 @@
-/*
-Michael Sloan
-CS 1181L-07
-Luke Holt
-February 13, 2022
-This program reads in a number of items (item class) from a .txt file and stores them as an ArrayList with other 
-items. Each of these ArrayLists is called a chromosome (chromosome class). The program performs numerous methods 
-in order to breed the most effective chromosome that can carry the greatest value of items while not exceeding 10 lbs.
-This program does this throughout many generations and once it is determined the most fit chromosome will be displayed
-to the console. This program works with items.txt as well as more_items.txt, but the population size, as well as the 
-generation size must be tweaked in order to eventually return the most desired chromosome. 
-*/
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
